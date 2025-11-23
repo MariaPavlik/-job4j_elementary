ab4d0e5e3c4bc7d0f4fcfc01f2f1be0399939331
MariaPavlik <shustalova@bk.ru>
2025-11-23 19:04:34 +0300

    3.2 Загрузка файлов на github [#505269 #555158]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args) {
+        System.out.println("Maria Pavlik");
+        System.out.println("30.12.1996");
+    }
+}
