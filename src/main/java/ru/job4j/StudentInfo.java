d00edf3039efa7750343880caff7c06aa21bff35
MariaPavlik <shustalova@bk.ru>
2025-11-23 19:05:44 +0300

    Разбор первой программы по "взрослому" [#505151]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main (String[] args) {
         System.out.println("Maria Pavlik");
-        System.out.println("30.12.1996");
+        System.out.println("30.12.96");
     }
 }
