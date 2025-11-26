5857a639ea18352c4b97b1d3a22016d3e0069742
MariaPavlik <shustalova@bk.ru>
2025-11-26 23:01:22 +0300

    Массивы и цикл for [#505223]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -4,7 +4,7 @@ public class ArrayLoop {
     public static void main(String[] args) {
         int[] mass = new int[10];
         for (int i = 0; i < mass.length; i++) {
-            mass[i] = i*2 + 3;
+            mass[i] = i * 2 + 3;
         }
         for (int j : mass) {
             System.out.println(j);
