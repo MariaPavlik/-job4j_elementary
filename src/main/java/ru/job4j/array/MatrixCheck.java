15edb781b96127344cb42de739ca8dfffbea1bde
MariaPavlik <shustalova@bk.ru>
2026-01-11 21:39:02 +0300

    6.7.4. Выигрышные комбинации в сокобан [#505248]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,17 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        char[] diagonal = extractDiagonal(board);
+        for (int i = 0; i < board.length; i++) {
+            if (diagonal[i] == 'X') {
+                if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
 }
